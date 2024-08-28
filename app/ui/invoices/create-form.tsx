9d8ae8d99789a5257cd6d9acfe8d5929e0e077a1
andrii.stetsiuk <zmontee@gmail.com>
2024-08-28 17:35:18 +0300

    server actions

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -7,10 +7,17 @@ import {
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
+import {createInvoice} from "@/app/lib/actions";
 
-export default function Form({ customers }: { customers: CustomerField[] }) {
+export default  function Form({ customers }: { customers: CustomerField[] }) {
+  const onSubmitHandler = async (formData: FormData) => {
+    'use server'
+    console.log('wtf');
+    createInvoice(formData);
+  }
+
   return (
-    <form>
+    <form action={onSubmitHandler}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
